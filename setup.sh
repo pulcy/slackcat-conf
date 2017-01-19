f1dbab43ae9612a6936cc5591cc4dfbca9cd22c2
Ewout Prangsma <ewout@prangsma.net>
2017-01-19 10:04:11 +0100

    Remove dir if found, sleep if configured todo so

--- setup.sh
+++ setup.sh
@@ -5,4 +5,12 @@ if [ -z ${WEBHOOK_URL} ]; then
     exit 1
 fi
 
+if [ -d /etc/slackcat.conf ]; then
+    rm -Rf /etc/slackcat.conf
+fi
+
 echo "{ \"webhook_url\": \"$WEBHOOK_URL\"}" > /etc/slackcat.conf
+
+if [ ! -z ${SLEEP} ]; then
+    sleep ${SLEEP}
+fi
